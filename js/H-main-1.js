fb55895c79520f079f44da8f3f20c84ea93ac41f
651619709@qq.com <651619709@qq.com>
2017-04-19 10:40:29 +0800

    H-V1.3

--- js/H-main-1.js
+++ js/H-main-1.js
@@ -1,5 +1,32 @@
 $(function(){
+//	大屏banner
 	$('.H-head-icon-list').on('touchstart',function(){
 		$('.H-list-box').slideToggle(200);
 	})
+	var now=0,next=0;
+	var ban=$('.H-banner-img');
+	var nav=$('.H-nav li');
+	function move(){
+		next=now+1;
+		if(next>ban.length-1){
+			next=0;
+		}
+		ban.eq(now).removeClass('first').end().eq(next).addClass('first');
+		nav.eq(now).removeClass('nav-active').end().eq(next).addClass('nav-active');
+		now=next;
+	}
+	var t=setInterval(move,3000);
+	ban.mousemove(function(){
+		clearInterval(t);
+	}).mouseout(function(){
+		t=setInterval(move,3000);
+	})
+	nav.click(function(){
+		$(this).clearQueue();
+		let index=$(this).index();
+			ban.eq(now).removeClass('first').end().eq(index).addClass('first');
+			nav.eq(now).removeClass('nav-active').end().eq(index).addClass('nav-active');
+			now=index;
+	})
+	
 })
